a95131d82d3d70d5c249a0d31ce1ac386f6d087f
Adrien M <adrien@d.tube>
2018-03-16 22:14:57 +0100

    allow cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 const port = process.env.PORT || 3000
 const jsonfile = require('jsonfile')
 const file = 'dmca.json'
