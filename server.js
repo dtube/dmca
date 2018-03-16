afba3870ce6604ae6a0d265be135d7374e43a9e2
Adrien M <adrien@d.tube>
2018-03-16 02:39:00 +0100

    add dmca service

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const jsonfile = require('jsonfile')
+const file = 'dmca.json'
+jsonfile.readFile(file, function(err, dmca) {
+  app.get('/v/:author/:permlink', (req, res) => {
+    if (dmca.authors.indexOf(req.params.author) > -1)
+        res.json({dmca: 2})
+    else if (dmca.videos.indexOf(req.params.author+'/'+req.params.permlink) > -1)
+        res.json({dmca: 1})
+    else
+        res.json({dmca: 0})
+  })
+  app.listen(port, () => console.log('DMCA server is ready on port '+port))
+})
